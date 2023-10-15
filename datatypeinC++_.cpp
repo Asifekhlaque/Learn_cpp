3af0b9f30f9c99d1151625e478aeca292e18c4eb
Asif Ekhlaque <132199879+Asifekhlaque@users.noreply.github.com>
2023-10-15 08:08:38 +0530

    Add files via upload

---
+++ datatypeinC++_.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string> 
+using namespace std;
+
+int main() {  
+    int a = 88;
+    char b = 'f';
+    float c = 77.8;
+    string d = "Khushi";
+    
+    cout << a << endl;
+    cout << b << " " << c << endl;  
+    cout << d << endl;
+    
+    return 0;  
+}
